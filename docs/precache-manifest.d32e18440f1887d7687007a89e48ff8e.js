49fe807b7f106970772b138aee1924530608cf16
ivywang122 <mucha1202@gmail.com>
2019-04-16 14:32:36 +0800

    update docs folder

---
+++ docs/precache-manifest.d32e18440f1887d7687007a89e48ff8e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "ac6768a685ef640bfa8cc94feba8113a",
+    "url": "/react-konvasjs-demo/static/media/canvas4.ac6768a6.png"
+  },
+  {
+    "revision": "2f75bb24979d81463ad757686d734ffb",
+    "url": "/react-konvasjs-demo/static/media/canvas3.2f75bb24.png"
+  },
+  {
+    "revision": "e6af9118274896b5ccaf7c84ba7e3394",
+    "url": "/react-konvasjs-demo/static/media/canvas2.e6af9118.png"
+  },
+  {
+    "revision": "d6432a4cbb74f4826439c623a29195b9",
+    "url": "/react-konvasjs-demo/static/media/canvas1.d6432a4c.png"
+  },
+  {
+    "revision": "0ccad1fb3016b5154f2e",
+    "url": "/react-konvasjs-demo/static/js/runtime~main.07cb2f8d.js"
+  },
+  {
+    "revision": "d6b958d8149da9549ad3",
+    "url": "/react-konvasjs-demo/static/js/main.0f356f99.chunk.js"
+  },
+  {
+    "revision": "f206bc5b8268df2dfc29",
+    "url": "/react-konvasjs-demo/static/js/2.39be09ad.chunk.js"
+  },
+  {
+    "revision": "d6b958d8149da9549ad3",
+    "url": "/react-konvasjs-demo/static/css/main.d7808066.chunk.css"
+  },
+  {
+    "revision": "4513ace9e5c0040754bde85eaffdb0a9",
+    "url": "/react-konvasjs-demo/index.html"
+  }
+];
